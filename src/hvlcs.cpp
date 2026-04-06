7969e6df14d79bda5663fbc809175633902897e1
Pranav921 <pranav.ar921@gmail.com>
2026-04-06 13:18:24 -0400

    Added backtracking for optimal sequence and example input and output

--- src/hvlcs.cpp
+++ src/hvlcs.cpp
@@ -30,5 +30,34 @@ pair<int, string> computeHVLCS(
         }
     }
 
-    return {dp[n][m], ""};
+    string subsequence;
+    int i = n;
+    int j = m;
+
+    while (i > 0 && j > 0) {
+        if (a[i - 1] == b[j - 1]) {
+            int charValue = 0;
+            auto it = values.find(a[i - 1]);
+            if (it != values.end()) {
+                charValue = it->second;
+            }
+
+            if (dp[i][j] == dp[i - 1][j - 1] + charValue) {
+                subsequence.push_back(a[i - 1]);
+                i--;
+                j--;
+                continue;
+            }
+        }
+
+        if (dp[i - 1][j] >= dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    reverse(subsequence.begin(), subsequence.end());
+
+    return {dp[n][m], subsequence};
 }
